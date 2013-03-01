f22395cc7d38e5eddb444f1378521484b34fc5d9
Cal Henderson <cal@iamcal.com>
2013-02-28 19:56:43 -0800

    added all the missing GMT offsets, verified from wikipedia city pages

--- lib_timezones.php
+++ lib_timezones.php
@@ -2,11 +2,15 @@
 	# sources:
 	# http://technet.microsoft.com/en-us/library/cc749073(v=ws.10).aspx (beware, has many mistakes)
 
+	function timezones_list(){
+
+		return array(
+
 (GMT-12:00) International Date Line West				Dateline Standard Time
 (GMT-11:00) Midway Island, Samoa		Samoa Standard Time
 (GMT-10:00) Hawaii				Hawaiian Standard Time
 (GMT-09:00) Alaska			Alaskan Standard Time
--8:00 Baja California
+(GMT-08:00) Baja California
 (GMT-08:00) Pacific Time (US and Canada); Tijuana		Pacific Standard Time
 (GMT-07:00) Arizona				US Mountain Standard Time
 (GMT-07:00) Chihuahua, La Paz, Mazatlan		Mountain Standard Time (Mexico)
@@ -18,19 +22,19 @@
 (GMT-05:00) Bogota, Lima, Quito			SA Pacific Standard Time
 (GMT-05:00) Eastern Time (US and Canada)	Eastern Standard Time
 (GMT-05:00) Indiana (East)			US Eastern Standard Time
--4:30 Caracas
--4:00 Asuncion
+(GMT-04:30) Caracas
+(GMT-04:00) Asuncion
 (GMT-04:00) Atlantic Time (Canada)	Atlantic Standard Time
--4:00 Cuiaba
+(GMT-04:00) Cuiaba
 (GMT-04:00) Georgetown, La Paz, Manaus, San Juan			SA Western Standard Time / Central Brazilian Standard Time
 (GMT-04:00) Santiago			Pacific SA Standard Time
 (GMT-03:30) Newfoundland and Labrador	Newfoundland Standard Time
 (GMT-03:00) Brasilia				E. South America Standard Time
 (GMT-03:00) Buenos Aires			SA Eastern Standard Time
--3:00 Cayenne, Fortaleza
+(GMT-03:00) Cayenne, Fortaleza
 (GMT-03:00) Greenland				Greenland Standard Time
--3:00 Montevideo
--3:00 Salvador
+(GMT-03:00) Montevideo
+(GMT-03:00) Salvador
 (GMT-02:00) Mid-Atlantic		Mid-Atlantic Standard Time
 (GMT-01:00) Azores			Azores Standard Time
 (GMT-01:00) Cape Verde Islands		Cape Verde Standard Time
@@ -43,34 +47,34 @@
 (GMT+01:00) West Central Africa			W. Central Africa Standard Time
 (GMT+01:00) Windhoek			Namibia Standard Time
 (GMT+02:00) Athens, Bucharest			GTB Standard Time
-Beirut
+(GMT+02:00) Beirut
 (GMT+02:00) Cairo				Egypt Standard Time
-Damascus
+(GMT+02:00) Damascus
 (GMT+02:00) Eastern Europe				E. Europe Standard Time
 (GMT+02:00) Harare, Pretoria			South Africa Standard Time
 (GMT+02:00) Helsinki, Kiev, Riga, Sofia, Tallinn, Vilnius	FLE Standard Time
-Istanbul
+(GMT+02:00) Istanbul
 (GMT+02:00) Jerusalem	Israel Standard Time
-Amman
+(GMT+03:00) Amman
 (GMT+03:00) Baghdad			Arabic Standard Time
-Kalinigrad, Minsk
+(GMT+03:00) Kalinigrad, Minsk
 (GMT+03:00) Kuwait, Riyadh		Arab Standard Time
 (GMT+03:00) Nairobi				E. Africa Standard Time
 (GMT+03:30) Tehran		Iran Standard Time
 (GMT+04:00) Abu Dhabi, Muscat		Arabian Standard Time
 (GMT+04:00) Baku			Azerbaijan Standard Time
 (GMT+04:00) Moscow, St. Petersburg, Volgograd		Russian Standard Time
-Port Louis
+(GMT+04:00) Port Louis
 (GMT+04:00) Tblisi						Georgian Standard Time
 (GMT+04:00) Yerevan			Caucasus Standard Time
 (GMT+04:30) Kabul			Afghanistan Standard Time
 (GMT+05:00) Islamabad, Karachi			West Asia Standard Time
-Tashkent
+(GMT+05:00) Tashkent
 (GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi		India Standard Time
 (GMT+05:30) Sri Jayawardenepura			Sri Lanka Standard Time
 (GMT+05:45) Kathmandu			Nepal Standard Time
 (GMT+06:00) Astana		Central Asia Standard Time
-Dhaka
+(GMT+06:00) Dhaka
 (GMT+06:00) Ekaterinburg			Ekaterinburg Standard Time
 (GMT+06:30) Yangon (Rangoon)		Myanmar Standard Time
 (GMT+07:00) Bangkok, Hanoi, Jakarta		SE Asia Standard Time
@@ -81,7 +85,7 @@ Dhaka
 (GMT+08:00) Perth				W. Australia Standard Time
 (GMT+08:00) Taipei				Taipei Standard Time
 (GMT+08:00) Ulaanbaatar	North Asia East Standard Time
-+9:00 Irkutsk
+(GMT+09:00) Irkutsk
 (GMT+09:00) Osaka, Sapporo, Tokyo		Tokyo Standard Time
 (GMT+09:00) Seoul		Korea Standard Time
 (GMT+09:30) Adelaide			Cen. Australia Standard Time
@@ -95,6 +99,6 @@ Dhaka
 (GMT+11:00) Vladivostok				Vladivostok Standard Time
 (GMT+12:00) Auckland, Wellington	New Zealand Standard Time
 (GMT+12:00) Fiji Islands, Kamchatka, Marshall Islands		Fiji Standard Time
-+12:00 Magadan
+(GMT+12:00) Magadan
 (GMT+13:00) Nuku'alofa				Tonga Standard Time
-+13:00 Samoa
+(GMT+13:00) Samoa
