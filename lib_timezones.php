088941f81018cc9dc55bf1385facf2a821cebe15
Cal Henderson <cal@iamcal.com>
2013-02-28 20:06:42 -0800

    indent and make into real php

--- lib_timezones.php
+++ lib_timezones.php
@@ -1,104 +1,109 @@
 <?php
-	# sources:
+	# Sources:
 	# http://technet.microsoft.com/en-us/library/cc749073(v=ws.10).aspx (beware, has many mistakes)
+	# http://www.worldtimezone.com/
+	# http://en.wikipedia.org/
+	# My ailing brain
 
 	function timezones_list(){
 
 		return array(
+			array('(GMT-12:00) International Date Line West',			'',	'Dateline Standard Time'),
+			array('(GMT-11:00) Midway Island, Samoa',				'',	'Samoa Standard Time'),
+			array('(GMT-10:00) Hawaii',						'',	'Hawaiian Standard Time'),
+			array('(GMT-09:00) Alaska',						'',	'Alaskan Standard Time'),
+			array('(GMT-08:00) Baja California',					'',	''),
+			array('(GMT-08:00) Pacific Time (US and Canada); Tijuana',		'',	'Pacific Standard Time'),
+			array('(GMT-07:00) Arizona',						'',	'US Mountain Standard Time'),
+			array('(GMT-07:00) Chihuahua, La Paz, Mazatlan',			'',	'Mountain Standard Time (Mexico)'),
+			array('(GMT-07:00) Mountain Time (US and Canada)',			'',	'Mountain Standard Time'),
+			array('(GMT-06:00) Central America',					'',	'Central America Standard Time'),
+			array('(GMT-06:00) Central Time (US and Canada)	',			'',	'Central Standard Time'),
+			array('(GMT-06:00) Guadalajara, Mexico City, Monterrey',		'',	'Central Standard Time (Mexico)'),
+			array('(GMT-06:00) Saskatchewan',					'',	'Canada Central Standard Time'),
+			array('(GMT-05:00) Bogota, Lima, Quito',				'',	'SA Pacific Standard Time'),
+			array('(GMT-05:00) Eastern Time (US and Canada)',			'',	'Eastern Standard Time'),
+			array('(GMT-05:00) Indiana (East)',					'',	'US Eastern Standard Time'),
+			array('(GMT-04:30) Caracas',						'',	''),
+			array('(GMT-04:00) Asuncion',						'',	''),
+			array('(GMT-04:00) Atlantic Time (Canada)',				'',	'Atlantic Standard Time'),
+			array('(GMT-04:00) Cuiaba',						'',	''),
+			array('(GMT-04:00) Georgetown, La Paz, Manaus, San Juan',		'',	'SA Western Standard Time / Central Brazilian Standard Time'),
+			array('(GMT-04:00) Santiago',						'',	'Pacific SA Standard Time'),
+			array('(GMT-03:30) Newfoundland and Labrador',				'',	'Newfoundland Standard Time'),
+			array('(GMT-03:00) Brasilia',						'',	'E. South America Standard Time'),
+			array('(GMT-03:00) Buenos Aires	',					'',	'SA Eastern Standard Time'),
+			array('(GMT-03:00) Cayenne, Fortaleza',					'',	''),
+			array('(GMT-03:00) Greenland',						'',	'Greenland Standard Time'),
+			array('(GMT-03:00) Montevideo',						'',	''),
+			array('(GMT-03:00) Salvador',						'',	''),
+			array('(GMT-02:00) Mid-Atlantic',					'',	'Mid-Atlantic Standard Time'),
+			array('(GMT-01:00) Azores',						'',	'Azores Standard Time'),
+			array('(GMT-01:00) Cape Verde Islands',					'',	'Cape Verde Standard Time'),
+			array('(GMT) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London',	'',	'GMT Standard Time'),
+			array('(GMT) Casablanca, Monrovia',					'',	'Greenwich Standard Time'),
+			array('(GMT+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna',	'',	'W. Europe Standard Time'),
+			array('(GMT+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague',	'',	'Central Europe Standard Time'),
+			array('(GMT+01:00) Brussels, Copenhagen, Madrid, Paris',		'',	'Romance Standard Time'),
+			array('(GMT+01:00) Sarajevo, Skopje, Warsaw, Zagreb',			'',	'Central European Standard Time'),
+			array('(GMT+01:00) West Central Africa',				'',	'W. Central Africa Standard Time'),
+			array('(GMT+01:00) Windhoek',						'',	'Namibia Standard Time'),
+			array('(GMT+02:00) Athens, Bucharest',					'',	'GTB Standard Time'),
+			array('(GMT+02:00) Beirut',						'',	''),
+			array('(GMT+02:00) Cairo',						'',	'Egypt Standard Time'),
+			array('(GMT+02:00) Damascus',						'',	''),
+			array('(GMT+02:00) Eastern Europe',					'',	'E. Europe Standard Time'),
+			array('(GMT+02:00) Harare, Pretoria',					'',	'South Africa Standard Time'),
+			array('(GMT+02:00) Helsinki, Kiev, Riga, Sofia, Tallinn, Vilnius',	'',	'FLE Standard Time'),
+			array('(GMT+02:00) Istanbul',						'',	''),
+			array('(GMT+02:00) Jerusalem',						'',	'Israel Standard Time'),
+			array('(GMT+03:00) Amman',						'',	''),
+			array('(GMT+03:00) Baghdad',						'',	'Arabic Standard Time'),
+			array('(GMT+03:00) Kalinigrad, Minsk',					'',	''),
+			array('(GMT+03:00) Kuwait, Riyadh',					'',	'Arab Standard Time'),
+			array('(GMT+03:00) Nairobi',						'',	'E. Africa Standard Time'),
+			array('(GMT+03:30) Tehran',						'',	'Iran Standard Time'),
+			array('(GMT+04:00) Abu Dhabi, Muscat',					'',	'Arabian Standard Time'),
+			array('(GMT+04:00) Baku',						'',	'Azerbaijan Standard Time'),
+			array('(GMT+04:00) Moscow, St. Petersburg, Volgograd',			'',	'Russian Standard Time'),
+			array('(GMT+04:00) Port Louis',						'',	''),
+			array('(GMT+04:00) Tblisi',						'',	'Georgian Standard Time'),
+			array('(GMT+04:00) Yerevan',						'',	'Caucasus Standard Time'),
+			array('(GMT+04:30) Kabul',						'',	'Afghanistan Standard Time'),
+			array('(GMT+05:00) Islamabad, Karachi',					'',	'West Asia Standard Time'),
+			array('(GMT+05:00) Tashkent',						'',	''),
+			array('(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi',		'',	'India Standard Time'),
+			array('(GMT+05:30) Sri Jayawardenepura',				'',	'Sri Lanka Standard Time'),
+			array('(GMT+05:45) Kathmandu',						'',	'Nepal Standard Time'),
+			array('(GMT+06:00) Astana',						'',	'Central Asia Standard Time'),
+			array('(GMT+06:00) Dhaka',						'',	''),
+			array('(GMT+06:00) Ekaterinburg',					'',	'Ekaterinburg Standard Time'),
+			array('(GMT+06:30) Yangon (Rangoon)',					'',	'Myanmar Standard Time'),
+			array('(GMT+07:00) Bangkok, Hanoi, Jakarta',				'',	'SE Asia Standard Time'),
+			array('(GMT+07:00) Novosibirsk',					'',	'Omsk Time'),
+			array('(GMT+08:00) Beijing, Chongqing, Hong Kong SAR, Urumqi',		'',	'China Standard Time'),
+			array('(GMT+08:00) Krasnoyarsk',					'',	'North Asia Standard Time'),
+			array('(GMT+08:00) Kuala Lumpur, Singapore',				'',	'Singapore Standard Time'),
+			array('(GMT+08:00) Perth',						'',	'W. Australia Standard Time'),
+			array('(GMT+08:00) Taipei',						'',	'Taipei Standard Time'),
+			array('(GMT+08:00) Ulaanbaatar',					'',	'North Asia East Standard Time'),
+			array('(GMT+09:00) Irkutsk',						'',	''),
+			array('(GMT+09:00) Osaka, Sapporo, Tokyo',				'',	'Tokyo Standard Time'),
+			array('(GMT+09:00) Seoul',						'',	'Korea Standard Time'),
+			array('(GMT+09:30) Adelaide',						'',	'Cen. Australia Standard Time'),
+			array('(GMT+09:30) Darwin',						'',	'AUS Central Standard Time'),
+			array('(GMT+10:00) Brisbane',						'',	'E. Australia Standard Time'),
+			array('(GMT+10:00) Canberra, Melbourne, Sydney',			'',	'AUS Eastern Standard Time'),
+			array('(GMT+10:00) Guam, Port Moresby',					'',	'West Pacific Standard Time'),
+			array('(GMT+10:00) Hobart',						'',	'Tasmania Standard Time'),
+			array('(GMT+10:00) Yakutsk',						'',	'Yakutsk Standard Time'),
+			array('(GMT+11:00) Solomon Islands, New Caledonia',			'',	'Central Pacific Standard Time'),
+			array('(GMT+11:00) Vladivostok',					'',	'Vladivostok Standard Time'),
+			array('(GMT+12:00) Auckland, Wellington',				'',	'New Zealand Standard Time'),
+			array('(GMT+12:00) Fiji Islands, Kamchatka, Marshall Islands',		'',	'Fiji Standard Time'),
+			array('(GMT+12:00) Magadan',						'',	''),
+			array('(GMT+13:00) Nuku\'alofa',					'',	'Tonga Standard Time'),
+			array('(GMT+13:00) Samoa',						'',	''),
+		);
+	}
 
-(GMT-12:00) International Date Line West				Dateline Standard Time
-(GMT-11:00) Midway Island, Samoa		Samoa Standard Time
-(GMT-10:00) Hawaii				Hawaiian Standard Time
-(GMT-09:00) Alaska			Alaskan Standard Time
-(GMT-08:00) Baja California
-(GMT-08:00) Pacific Time (US and Canada); Tijuana		Pacific Standard Time
-(GMT-07:00) Arizona				US Mountain Standard Time
-(GMT-07:00) Chihuahua, La Paz, Mazatlan		Mountain Standard Time (Mexico)
-(GMT-07:00) Mountain Time (US and Canada)		Mountain Standard Time
-(GMT-06:00) Central America		Central America Standard Time
-(GMT-06:00) Central Time (US and Canada)				Central Standard Time
-(GMT-06:00) Guadalajara, Mexico City, Monterrey				Central Standard Time (Mexico)
-(GMT-06:00) Saskatchewan		Canada Central Standard Time
-(GMT-05:00) Bogota, Lima, Quito			SA Pacific Standard Time
-(GMT-05:00) Eastern Time (US and Canada)	Eastern Standard Time
-(GMT-05:00) Indiana (East)			US Eastern Standard Time
-(GMT-04:30) Caracas
-(GMT-04:00) Asuncion
-(GMT-04:00) Atlantic Time (Canada)	Atlantic Standard Time
-(GMT-04:00) Cuiaba
-(GMT-04:00) Georgetown, La Paz, Manaus, San Juan			SA Western Standard Time / Central Brazilian Standard Time
-(GMT-04:00) Santiago			Pacific SA Standard Time
-(GMT-03:30) Newfoundland and Labrador	Newfoundland Standard Time
-(GMT-03:00) Brasilia				E. South America Standard Time
-(GMT-03:00) Buenos Aires			SA Eastern Standard Time
-(GMT-03:00) Cayenne, Fortaleza
-(GMT-03:00) Greenland				Greenland Standard Time
-(GMT-03:00) Montevideo
-(GMT-03:00) Salvador
-(GMT-02:00) Mid-Atlantic		Mid-Atlantic Standard Time
-(GMT-01:00) Azores			Azores Standard Time
-(GMT-01:00) Cape Verde Islands		Cape Verde Standard Time
-(GMT) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London	GMT Standard Time
-(GMT) Casablanca, Monrovia			Greenwich Standard Time
-(GMT+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna			W. Europe Standard Time
-(GMT+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague		Central Europe Standard Time
-(GMT+01:00) Brussels, Copenhagen, Madrid, Paris			Romance Standard Time
-(GMT+01:00) Sarajevo, Skopje, Warsaw, Zagreb				Central European Standard Time
-(GMT+01:00) West Central Africa			W. Central Africa Standard Time
-(GMT+01:00) Windhoek			Namibia Standard Time
-(GMT+02:00) Athens, Bucharest			GTB Standard Time
-(GMT+02:00) Beirut
-(GMT+02:00) Cairo				Egypt Standard Time
-(GMT+02:00) Damascus
-(GMT+02:00) Eastern Europe				E. Europe Standard Time
-(GMT+02:00) Harare, Pretoria			South Africa Standard Time
-(GMT+02:00) Helsinki, Kiev, Riga, Sofia, Tallinn, Vilnius	FLE Standard Time
-(GMT+02:00) Istanbul
-(GMT+02:00) Jerusalem	Israel Standard Time
-(GMT+03:00) Amman
-(GMT+03:00) Baghdad			Arabic Standard Time
-(GMT+03:00) Kalinigrad, Minsk
-(GMT+03:00) Kuwait, Riyadh		Arab Standard Time
-(GMT+03:00) Nairobi				E. Africa Standard Time
-(GMT+03:30) Tehran		Iran Standard Time
-(GMT+04:00) Abu Dhabi, Muscat		Arabian Standard Time
-(GMT+04:00) Baku			Azerbaijan Standard Time
-(GMT+04:00) Moscow, St. Petersburg, Volgograd		Russian Standard Time
-(GMT+04:00) Port Louis
-(GMT+04:00) Tblisi						Georgian Standard Time
-(GMT+04:00) Yerevan			Caucasus Standard Time
-(GMT+04:30) Kabul			Afghanistan Standard Time
-(GMT+05:00) Islamabad, Karachi			West Asia Standard Time
-(GMT+05:00) Tashkent
-(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi		India Standard Time
-(GMT+05:30) Sri Jayawardenepura			Sri Lanka Standard Time
-(GMT+05:45) Kathmandu			Nepal Standard Time
-(GMT+06:00) Astana		Central Asia Standard Time
-(GMT+06:00) Dhaka
-(GMT+06:00) Ekaterinburg			Ekaterinburg Standard Time
-(GMT+06:30) Yangon (Rangoon)		Myanmar Standard Time
-(GMT+07:00) Bangkok, Hanoi, Jakarta		SE Asia Standard Time
-(GMT+07:00) Novosibirsk				Omsk Time
-(GMT+08:00) Beijing, Chongqing, Hong Kong SAR, Urumqi			China Standard Time
-(GMT+08:00) Krasnoyarsk			North Asia Standard Time
-(GMT+08:00) Kuala Lumpur, Singapore		Singapore Standard Time
-(GMT+08:00) Perth				W. Australia Standard Time
-(GMT+08:00) Taipei				Taipei Standard Time
-(GMT+08:00) Ulaanbaatar	North Asia East Standard Time
-(GMT+09:00) Irkutsk
-(GMT+09:00) Osaka, Sapporo, Tokyo		Tokyo Standard Time
-(GMT+09:00) Seoul		Korea Standard Time
-(GMT+09:30) Adelaide			Cen. Australia Standard Time
-(GMT+09:30) Darwin			AUS Central Standard Time
-(GMT+10:00) Brisbane				E. Australia Standard Time
-(GMT+10:00) Canberra, Melbourne, Sydney	AUS Eastern Standard Time
-(GMT+10:00) Guam, Port Moresby				West Pacific Standard Time
-(GMT+10:00) Hobart				Tasmania Standard Time
-(GMT+10:00) Yakutsk				Yakutsk Standard Time
-(GMT+11:00) Solomon Islands, New Caledonia			Central Pacific Standard Time
-(GMT+11:00) Vladivostok				Vladivostok Standard Time
-(GMT+12:00) Auckland, Wellington	New Zealand Standard Time
-(GMT+12:00) Fiji Islands, Kamchatka, Marshall Islands		Fiji Standard Time
-(GMT+12:00) Magadan
-(GMT+13:00) Nuku'alofa				Tonga Standard Time
-(GMT+13:00) Samoa
