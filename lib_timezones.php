2dc5010eec1ce8719f4482d270abef4d35e4e6c2
Cal Henderson <cal@iamcal.com>
2014-10-28 16:07:17 -0700

    the big russian timezone change of october 26th, 2014

--- lib_timezones.php
+++ lib_timezones.php
@@ -62,7 +62,6 @@
 			array('(UTC+01:00) Sarajevo, Skopje, Warsaw, Zagreb',			'Europe/Warsaw',		false,	'Central European Time',		'Central European Summer Time'),
 			array('(UTC+01:00) West Central Africa',				'Africa/Algiers',		false,	'Central European Time',		null),
 			array('(UTC+01:00) Windhoek',						'Africa/Windhoek',		false,	'West Africa Time',			'West Africa Summer Time'),
-			array('(UTC+01:00) Tripoli',						'Africa/Tripoli',		false,	'Central European Time',		'Central European Summer Time'),
 			array('(UTC+02:00) Athens, Bucharest',					'Europe/Athens',		false,	'Eastern European Time',		'Eastern European Summer Time'),
 			array('(UTC+02:00) Beirut',						'Asia/Beirut',			false,	'Eastern European Time',		'Eastern European Summer Time'),
 			array('(UTC+02:00) Cairo',						'Africa/Cairo',			false,	'Eastern European Time',		'Eastern European Summer Time'),
@@ -72,51 +71,56 @@
 			array('(UTC+02:00) Helsinki, Kiev, Riga, Sofia, Tallinn, Vilnius',	'Europe/Helsinki',		false,	'Eastern European Time',		'Eastern European Summer Time'),
 			array('(UTC+02:00) Istanbul',						'Asia/Istanbul',		false,	'Eastern European Time',		'Eastern European Summer Time'),
 			array('(UTC+02:00) Jerusalem',						'Asia/Jerusalem',		false,	'Israel Standard Time',			'Israel Daylight Time'),
+			array('(UTC+02:00) Kaliningrad',					'Europe/Kaliningrad',		false,	'Eastern European Time',		null), # RTZ1
+			array('(UTC+02:00) Tripoli',						'Africa/Tripoli',		false,	'Eastern European Time',		null),
 			array('(UTC+03:00) Amman',						'Asia/Amman',			false,	'Arabia Standard Time',			null),
 			array('(UTC+03:00) Baghdad',						'Asia/Baghdad',			false,	'Arabia Standard Time',			null),
-			array('(UTC+03:00) Kaliningrad, Minsk',					'Europe/Kaliningrad',		false,	'Kaliningrad Time',			null), # has no name on timeanddate.com, no DST since 2011
 			array('(UTC+03:00) Kuwait, Riyadh',					'Asia/Kuwait',			true,	'Arabia Standard Time',			null),
+			array('(UTC+03:00) Minsk',						'Europe/Minsk',			false,	'Further-eastern European Time',	null),
+			array('(UTC+03:00) Moscow, St. Petersburg, Volgograd',			'Europe/Moscow',		true,	'Moscow Time',				null), # RTZ2
 			array('(UTC+03:00) Nairobi',						'Africa/Nairobi',		false,	'East Africa Time',			null),
 			array('(UTC+03:30) Tehran',						'Asia/Tehran',			false,	'Iran Standard Time',			'Iran Daylight Time'),
 			array('(UTC+04:00) Abu Dhabi, Muscat',					'Asia/Muscat',			true,	'Gulf Standard Time',			null),
 			array('(UTC+04:00) Baku',						'Asia/Baku',			true,	'Azerbaijan Time',			'Azerbaijan Summer Time'),
-			array('(UTC+04:00) Moscow, St. Petersburg, Volgograd',			'Europe/Moscow',		true,	'Moscow Standard Time',			'Moscow Daylight Time'),
+			array('(UTC+04:00) Izhevsk, Samara',					'Europe/Samara',		false,	'Samara Time',				null), # RTZ3
 			array('(UTC+04:00) Port Louis',						'Indian/Mauritius',		false,	'Mauritius Time',			null),
 			array('(UTC+04:00) Tbilisi',						'Asia/Tbilisi',			false,	'Georgia Standard Time',		null),
 			array('(UTC+04:00) Yerevan',						'Asia/Yerevan',			false,	'Armenia Time',				'Armenia Summer Time'),
 			array('(UTC+04:30) Kabul',						'Asia/Kabul',			false,	'Afghanistan Time',			null),
 			array('(UTC+05:00) Tashkent, Ashgabat',					'Asia/Tashkent',		true,	'Uzbekistan Time',			null),
+			array('(UTC+05:00) Ekaterinburg',					'Asia/Yekaterinburg',		false,	'Yekaterinburg Time',			null), # RTZ4
 			array('(UTC+05:00) Islamabad, Karachi',					'Asia/Karachi',			false,	'Pakistan Standard Time',		null),
 			array('(UTC+05:30) Chennai, Kolkata, Mumbai, New Delhi',		'Asia/Kolkata',			false,	'India Standard Time',			null),
 			array('(UTC+05:30) Sri Jayawardenepura',				'Asia/Colombo',			false,	'ISri Lanka Standard Time',		null), # matches India Standard Time since 2006
 			array('(UTC+05:45) Kathmandu',						'Asia/Katmandu',		false,	'Nepal Time',				null),
 			array('(UTC+06:00) Astana',						'Asia/Almaty',			false,	'Alma-Ata Time',			null),
 			array('(UTC+06:00) Dhaka',						'Asia/Dhaka',			true,	'Bangladesh Standard Time',		null),
-			array('(UTC+06:00) Ekaterinburg',					'Asia/Yekaterinburg',		false,	'Yekaterinburg Time',			'Yekaterinburg Summer Time'),
+			array('(UTC+06:00) Novosibirsk',					'Asia/Novosibirsk',		false,	'Novosibirsk Time',			null), # RTZ5
 			array('(UTC+06:30) Yangon (Rangoon)',					'Asia/Rangoon',			false,	'Myanmar Time',				null),
 			array('(UTC+07:00) Bangkok, Hanoi, Jakarta',				'Asia/Bangkok',			false,	'Indochina Time',			null),
-			array('(UTC+07:00) Novosibirsk',					'Asia/Novosibirsk',		false,	'Novosibirsk Time',			'Novosibirsk Summer Time'),
+			array('(UTC+07:00) Krasnoyarsk',					'Asia/Krasnoyarsk',		false,	'Krasnoyarsk Time',			null), # RTZ6
 			array('(UTC+08:00) Beijing, Chongqing, Hong Kong SAR, Urumqi',		'Asia/Chongqing',		true,	'China Standard Time',			null),
-			array('(UTC+08:00) Krasnoyarsk',					'Asia/Krasnoyarsk',		false,	'Krasnoyarsk Time',			'Krasnoyarsk Summer Time'),
+			array('(UTC+08:00) Irkutsk',						'Asia/Irkutsk',			false,	'Irkutsk Time',				null), # RTZ7
 			array('(UTC+08:00) Kuala Lumpur, Singapore',				'Asia/Kuala_Lumpur',		false,	'Singapore Standard Time',		null), # Malaysia Time
 			array('(UTC+08:00) Perth',						'Australia/Perth',		false,	'Australian Western Standard Time',	null),
 			array('(UTC+08:00) Taipei',						'Asia/Taipei',			false,	'Taiwan Standard Time',			null),
 			array('(UTC+08:00) Ulaanbaatar',					'Asia/Ulaanbaatar',		true,	'Ulaanbaatar Time',			null),
-			array('(UTC+09:00) Irkutsk',						'Asia/Irkutsk',			false,	'Irkutsk Time',				'Irkutsk Summer Time'), # no DST since 2011
 			array('(UTC+09:00) Osaka, Sapporo, Tokyo',				'Asia/Tokyo',			true,	'Japan Standard Time',			null),
 			array('(UTC+09:00) Seoul',						'Asia/Seoul',			false,	'Korea Standard Time',			null),
+			array('(UTC+09:00) Yakutsk',						'Asia/Yakutsk',			false,	'Yakutsk Time',				null), # RTZ8
 			array('(UTC+09:30) Adelaide',						'Australia/Adelaide',		false,	'Australian Central Standard Time',	'Australian Central Daylight Time'),
 			array('(UTC+09:30) Darwin',						'Australia/Darwin',		false,	'Australian Central Standard Time',	'Australian Central Daylight Time'),
 			array('(UTC+10:00) Brisbane',						'Australia/Brisbane',		true,	'Australian Eastern Standard Time',	'Australian Eastern Daylight Time'),
 			array('(UTC+10:00) Canberra, Melbourne, Sydney',			'Australia/Canberra',		true,	'Australian Eastern Standard Time',	'Australian Eastern Daylight Time'),
 			array('(UTC+10:00) Guam, Port Moresby',					'Pacific/Guam',			false,	'West Pacific Standard Time',		null), # Chamorro Standard Time / Papua New Guinea Time
 			array('(UTC+10:00) Hobart',						'Australia/Hobart',		false,	'Australian Eastern Standard Time',	'Australian Eastern Daylight Time'),
-			array('(UTC+10:00) Yakutsk',						'Asia/Yakutsk',			false,	'Yakutsk Time',				'Yakutsk Summer Time'),
+			array('(UTC+10:00) Magadan',						'Asia/Magadan',			false,	'Vladivostok Time',			null),
+			array('(UTC+10:00) Vladivostok, Magadan',				'Asia/Vladivostok',		false,	'Vladivostok Time',			null), # RTZ9
+			array('(UTC+11:00) Chokirdakh',						'Asia/Srednekolymsk',		false,	'Srednekolymsk Time',			null), # RTZ10
 			array('(UTC+11:00) Solomon Islands, New Caledonia',			'Pacific/Guadalcanal',		false,	'Central Pacific Standard Time',	null), # Solomon IslandsTime / New Caledonia Time
-			array('(UTC+11:00) Vladivostok',					'Asia/Vladivostok',		false,	'Vladivostok Time',			'Vladivostok Summer Time'),
+			array('(UTC+12:00) Anadyr, Petropavlovsk-Kamchatsky',			'Asia/Anadyr',			false,	'Kamchatka Time',			null), # RTZ11
 			array('(UTC+12:00) Auckland, Wellington',				'Pacific/Auckland',		false,	'New Zealand Standard Time',		'New Zealand Daylight Time'),
 			array('(UTC+12:00) Fiji Islands, Kamchatka, Marshall Islands',		'Pacific/Fiji',			false,	'Fiji Time',				'Fiji Summer Time'),
-			array('(UTC+12:00) Magadan',						'Asia/Magadan',			false,	'Magadan Time',				'Magadan Summer Time'),
 			array('(UTC+13:00) Nuku\'alofa',					'Pacific/Tongatapu',		false,	'Tonga Standard Time',			null),
 			array('(UTC+13:00) Samoa',						'Pacific/Apia',			false,	'West Samoa Time',			null), # DST name is same as non
 		);
