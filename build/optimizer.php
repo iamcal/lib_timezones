20fd5cb6df95e0101b9c6b4c0d88e9b6be0ef1c7
Cal Henderson <cal@iamcal.com>
2016-06-22 17:43:21 -0500

    moved the unmatched dupes code into its own tool

--- build/optimizer.php
+++ build/optimizer.php
@@ -3,33 +3,6 @@
 
 
 	#
-	# dump unmatched
-	#
-
-	if (false){
-
-		$map = build_map($zones, $base_dates);
-
-		$per = count($map) / count($zones);
-		$per = round($per * 1000) / 10;
-
-		echo "number of zones in list  : ".count($zones)."\n";
-		echo "number of zones detected : ".count($map)."\n";
-		echo "match rate : {$per}%\n";
-		echo "\n";
-
-		foreach ($map as $k => $v){
-			if (count($v) == 1) continue;
-			foreach (array_slice($v, 1) as $v2){
-				echo "Can't detect $v2 (counts as $v[0])\n";
-			}
-		}
-
-		exit;
-	}
-
-
-	#
 	# optimizer
 	#
 
