975901e892a02e1d3f6d1fa0c83d8440e5a1ca6a
Cal Henderson <cal@iamcal.com>
2016-06-21 19:46:17 -0500

    added brute forcing to find the best dates for detection. selected a new set of 10 dates that gets us to 97/102 zones. changed the way the template is used to allow key building and flexible dates.

--- build/gen_js.php
+++ build/gen_js.php
@@ -5,64 +5,208 @@
 
 	date_default_timezone_set('America/Los_Angeles');
 
-	$dates = array(
-		array(0,0,0,6,30,2012),
-		array(0,0,0,12,30,2012),
+
+	function build_map($zones, $dates){
+
+		#
+		# create each probe date
+		#
+
+		$probe_dates = array();
+		foreach ($dates as $d){
+			$date = date_create();
+			date_timestamp_set($date, gmmktime($d[0], $d[1], $d[2], $d[3], $d[4], $d[5]));
+			$probe_dates[] = $date;
+		}
+
+
+		#
+		# build the map
+		#
+
+		$map = array();
+
+		foreach ($zones as $row){
+
+			$tz = timezone_open($row[1]);
+			$bits = array();
+
+			foreach ($probe_dates as $d){
+				date_timezone_set($d, $tz);
+				$off = date_offset_get($d) / 60;
+				$bits[] = $off;
+			}
+
+			$key = implode(':', $bits);
+
+			if (array_key_exists($key, $map)){
+				if ($row[2]){
+					array_unshift($map[$key], $row[1]);
+				}else{
+					$map[$key][] = $row[1];
+				}
+			}else{
+				$map[$key] = array($row[1]);
+			}
+		}
+
+		return $map;
+	}
+
+
+
+	$zones = timezones_list();
+
+	$base_dates = array(
+		array(0,0,0,12,31,2011),
+		array(0,0,0,9,29,1991),
+		array(0,0,0,10,7,1996),
+		array(0,0,0,10,27,2008),
+		array(0,0,0,4,3,2011),
+		array(0,0,0,4,28,1980),
+		array(0,0,0,3,26,1989),
+		array(0,0,0,5,10,1987),
+		array(0,0,0,4,6,1980),
+		array(0,0,0,12,19,1982),
 	);
 
 
+	#
+	# dump unmatched
+	#
 
-	$probe_dates = array();
-	foreach ($dates as $d){
-		$date = date_create();
-		date_timestamp_set($date, gmmktime($d[0], $d[1], $d[2], $d[3], $d[4], $d[5]));
-		$probe_dates[] = $date;
-	}
+	if (false){
 
-	$zones = timezones_list();
-	$map = array();
+		$map = build_map($zones, $base_dates);
 
-	foreach ($zones as $row){
+		$per = count($map) / count($zones);
+		$per = round($per * 1000) / 10;
 
-		$tz = timezone_open($row[1]);
-		$bits = array();
+		echo "number of zones in list  : ".count($zones)."\n";
+		echo "number of zones detected : ".count($map)."\n";
+		echo "match rate : {$per}%\n";
+		echo "\n";
 
-		foreach ($probe_dates as $d){
-			date_timezone_set($d, $tz);
-			$off = date_offset_get($d) / 60;
-			$bits[] = $off;
+		foreach ($map as $k => $v){
+			if (count($v) == 1) continue;
+			foreach (array_slice($v, 1) as $v2){
+				echo "Can't detect $v2 (counts as $v[0])\n";
+			}
 		}
 
-		$key = implode(':', $bits);
+		exit;
+	}
 
-		if ($row[2]){
-			$map[$key] = $row[1];
-		}else{
-			if (!array_key_exists($key, $map)) $map[$key] = $row[1];
+
+	#
+	# optimizer
+	#
+
+	if (false){
+
+		$best = array();
+
+		for ($i=0; $i<7300; $i++){
+
+			$d = array(0,0,0,1,1+$i,1995);
+
+			$dates = $base_dates;
+			$dates[] = $d;
+
+			$k = date('Y-m-d', mktime($d[0], $d[1], $d[2], $d[3], $d[4], $d[5]));
+
+			$map = build_map($zones, $dates);
+			$num = count($map);
+			$best[$num][] = $k;
+		}
+
+		ksort($best);
+
+		foreach ($best as $k => $v){
+			echo "$k - ".implode(',', $v)."\n";
+		}
+
+		exit;
+	}
+
+
+	#
+	# brute crazy optimizer
+	#
+
+	if (false){
+
+		$keep_dates = array();
+
+		while (1){
+
+			$best = array();
+
+			for ($i=0; $i<35*365; $i++){
+
+				$d = array(0,0,0,1,1+$i,1980);
+
+				$dates = $keep_dates;
+				$dates[] = $d;
+
+				$k = gmdate('Y-m-d', gmmktime($d[0], $d[1], $d[2], $d[3], $d[4], $d[5]));
+
+				$map = build_map($zones, $dates);
+				$num = count($map);
+				$best[$num][] = $k;
+			}
+
+			$keys = array_keys($best);
+			sort($keys);
+			$best_key = array_pop($keys);
+			$best_d = $best[$best_key][0];
+
+			$r = count($keep_dates);
+			echo "round $r: best date was {$best_d}, matching {$best_key} uniques\n";
+
+			list($y, $m, $d) = explode('-', $best_d);
+			$keep_dates[] = array(0,0,0,$m,$d,$y);
 		}
 	}
 
-	$per = count($map) / count($zones);
-	$per = round($per * 1000) / 10;
-
-	echo "number of zones in list  : ".count($zones)."\n";
-	echo "number of zones detected : ".count($map)."\n";
-	echo "match rate : {$per}%\n";
-	exit;
 
 
-	$lines = file('timezones.js.template');
-	echo implode('', $lines);
+	#
+	# build our output blocks
+	#
+
+	$map = build_map($zones, $base_dates);
 
 
-	# do our own pretty printing to this works
-	# on older PHP.
+	$dates = '';
+	foreach ($base_dates as $d){
+		$dates .= "\tkey_parts.push(-1 * new Date(Date.UTC({$d[5]}, {$d[3]}, {$d[4]}, {$d[0]}, {$d[1]}, {$d[2]}, 0)).getTimezoneOffset());\n";
+	}
 
 	$map_lines = array();
-	foreach ($map as $k => $v) $map_lines[] = "\t'$k':\t'$v'";
+	foreach ($map as $k => $v) $map_lines[] = "\t'$k':\t'{$v[0]}'";
+	$map = "{\n".implode(",\n", $map_lines)."\n}";
+
 
 	$zone_lines = array();
 	foreach ($zones as $row) $zone_lines[] = "\t[\"{$row[0]}\", '{$row[1]}']";
+	$list = "[\n".implode(",\n", $zone_lines)."\n]";
 
-	echo "\nvar _timezones_map = {\n".implode(",\n", $map_lines)."\n};\n";
-	echo "\nvar _timezones_list = [\n".implode(",\n", $zone_lines)."\n];\n";
+
+	#
+	# output
+	#
+
+	$template = file_get_contents('timezones.js.template');
+
+	$template = str_replace(array(
+		'#DATES#',
+		'#MAP#',
+		'#LIST#',
+	), array(
+		$dates,
+		$map,
+		$list,
+	), $template);
+
+	echo $template;
