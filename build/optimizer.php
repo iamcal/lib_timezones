32d4cfe50e3ab1b68eae4e56f7d0ada588929579
Cal Henderson <cal@iamcal.com>
2016-06-22 17:42:59 -0500

    support hours in the optimizer

--- build/optimizer.php
+++ build/optimizer.php
@@ -2,23 +2,6 @@
 	include('lib.php');
 
 
-	$zones = timezones_list();
-
-	$base_dates = array(
-		array(0,0,0,12,10,2007),
-		array(0,0,0,04,27,1981),
-		array(0,0,0,05,25,1996),
-		array(0,0,0,03,10,1986),
-		array(0,0,0,10,26,2014),
-		array(0,0,0,10,28,1990),
-		array(0,0,0,03,29,1981),
-		array(0,0,0,05,08,1988),
-		array(0,0,0,09,26,1993),
-		array(0,0,0,12,19,1982),
-		array(0,0,0,03,30,2007),
-	);
-
-
 	#
 	# dump unmatched
 	#
@@ -90,18 +73,20 @@
 
 			$best = array();
 
-			for ($i=0; $i<15*365; $i++){
+			for ($j=0; $j<24; $j+=6){
+			for ($i=0; $i<20*365; $i++){
 
-				$d = array(0,0,0,1,1+$i,2000);
+				$d = array($j,0,0,1,1+$i,2000);
 
 				$dates = $keep_dates;
 				$dates[] = $d;
 
-				$k = gmdate('Y-m-d', gmmktime($d[0], $d[1], $d[2], $d[3], $d[4], $d[5]));
+				$k = gmdate('H-i-s-m-d-Y', gmmktime($d[0],$d[1],$d[2],$d[3],$d[4],$d[5]));
 
 				$map = build_map($zones, $dates);
 				$num = count($map);
 				$best[$num][] = $k;
+			}
 			}
 
 			$keys = array_keys($best);
@@ -109,10 +94,11 @@
 			$best_key = array_pop($keys);
 			$best_d = $best[$best_key][0];
 
-			$r = count($keep_dates);
-			echo "round $r: best date was {$best_d}, matching {$best_key} uniques\n";
+			$d = explode('-', $best_d);
 
-			list($y, $m, $d) = explode('-', $best_d);
-			$keep_dates[] = array(0,0,0,$m,$d,$y);
+			$r = count($keep_dates);
+			echo "round $r: best date was {$best_d}, matching {$best_key} uniques - array($d[0],$d[1],$d[2],$d[3],$d[4],$d[5]),\n";
+
+			$keep_dates[] = $d;
 		}
 	}
