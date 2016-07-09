b1c31c79304abcdd5271e4fba1fb98708a0a53b5
Cal Henderson <cal@iamcal.com>
2016-07-09 16:34:18 -0500

    added a fallback mapping

--- data/static.php
+++ data/static.php
@@ -178,6 +178,7 @@
 	# would be better than just giving up/using a default.
 
 	$timezones_fallback_map = array(
+		"America/Fort_Nelson"	=> "America/Denver", # switched to mountain time Mar 8th 2015
 	);
 
 
