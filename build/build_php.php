693952e2bd44e73c384244876b634303b38f2261
Cal Henderson <cal@iamcal.com>
2016-07-09 14:51:14 -0500

    fully generate the `timezones_filter()` php function using the 4 mappings (choices, manual, auto, fallbacks)

--- build/build_php.php
+++ build/build_php.php
@@ -7,42 +7,45 @@
 	include(dirname(__FILE__)."/../data/dynamic.php");
 
 
-	#
-	# build the output lines
-	#
-
-	if (count($timezones_nomap) || count($timezones_nomap_obsolete)){
-		$broken = "// The follow zone IDs have no mapping to our choice list:\n";
-		foreach ($timezones_nomap as $id){
-			$broken .= "// $id\n";
-		}
-		foreach ($timezones_nomap_obsolete as $id){
-			$broken .= "// $id (Obsolete)\n";
-		}
-	}
-
 
 	#
 	# build up our output
 	#
 
-	$map = "\$map = array(\n";
-	foreach ($timezones_exact_map as $k => $v){
-		$map .= "\t\t\t\"$k\" => \"$v\",\n";
+	$zone_choices = "array(\n";
+	foreach ($timezones_list as $row){
+		$zone_choices .= "\t\t\t".var_export($row[1], true).",\n";
 	}
-	$map .= "\t\t);\n";
+	$zone_choices .= "\t\t)";
+
+
+	function local_format_map($map){
+		$out = "array(\n";
+		foreach ($map as $k => $v){
+			$out .= "\t\t\t".var_export($k, true)." => ".var_export($v, true).",\n";
+		}
+		return $out."\t\t)";
+	}
+
+	$manual_map = local_format_map($timezones_manual_map);
+	$auto_map = local_format_map($timezones_exact_map);
+	$fallback_map = local_format_map($timezones_fallback_map);
 
 
 	#
 	# output
 	#
 
+	$vars = array(
+		'#ZONE-CHOICES#'	=> $zone_choices,
+		'#MANUAL-MAP#'		=> $manual_map,
+		'#AUTO-MAP#'		=> $auto_map,
+		'#FALLBACK-MAP"'	=> $fallback_map,
+	);
+
 	$template = file_get_contents(dirname(__FILE__).'/timezones.php.template');
 
-	$template = str_replace(array(
-		'#MAP#',
-	), array(
-		$map,
-	), $template);
+	$template = str_replace(array_keys($vars), $vars, $template);
 
-	echo $template;
+	$fh = fopen(dirname(__FILE__).'/../lib/lib_timezones.php', 'w');
+	fputs($fh, $template);
